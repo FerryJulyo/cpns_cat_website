c262c0914921598e91561fc25423649594321785
FerryJulyo <ferryjulyo86@gmail.com>
2025-11-05 15:55:50 +0700

    Create RegisterController.php

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'nik' => 'required|string|size:16|unique:users',
+            'phone' => 'required|string|max:15',
+            'birth_date' => 'required|date',
+            'gender' => 'required|in:L,P',
+            'address' => 'required|string',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'nik' => $request->nik,
+            'phone' => $request->phone,
+            'birth_date' => $request->birth_date,
+            'gender' => $request->gender,
+            'address' => $request->address,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->login($user);
+
+        return redirect()->route('dashboard')->with('success', 'Registrasi berhasil! Selamat datang.');
+    }
+}
