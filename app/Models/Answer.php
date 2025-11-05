eec295c76bf04f55b0602bc492fb086e79ed75b5
FerryJulyo <ferryjulyo86@gmail.com>
2025-11-05 15:55:57 +0700

    Create Answer.php

---
+++ app/Models/Answer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    protected $fillable = [
+        'exam_session_id', 'question_id', 'user_answer', 'is_correct'
+    ];
+
+    public function examSession()
+    {
+        return $this->belongsTo(ExamSession::class);
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+}
