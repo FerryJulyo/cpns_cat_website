1262cc80e6ad72b84c6f4c20e5a8ad19fe6c3636
FerryJulyo <ferryjulyo86@gmail.com>
2025-11-06 13:10:16 +0700

    Update ExamController.php

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -11,10 +11,10 @@ use Carbon\Carbon;
 
 class ExamController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     public function show($id)
     {
